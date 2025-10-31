5a8de17e2538972767b339040e09edcbde921e94
Tona <tona.chheun@akilabs.co>
2025-10-31 10:38:32 +0700

    init

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+
+export default function AboutPage() {
+    return (
+        <>
+            <h1>About Us</h1>
+            <p>This is the About page of our Next.js app.</p>
+        </>
+    );
+}
---
+++ app/post/page.tsx
@@ -0,0 +1,6 @@
+
+export default function PostPage() {
+    return (
+        <h1>Hello Post Article</h1>
+    )
+}
